c5747353f1201c54ed259e2a8c54d6ad2b2254dc
NguyenCaoDuyLinh <linhgood04@gmail.com>
2023-01-05 23:00:16 +0700

    Login_DL

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+// hello
 Route::prefix('auth')->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::post('login', 'login');
